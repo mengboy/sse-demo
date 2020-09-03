5bf9e72fb5783bb8552be63b71d2220246d64456
baimengfan <baimengfan@xiaomi.com>
2020-09-03 10:15:15 +0800

    feat: commit

---
+++ main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Broker struct {
+
+	// Events are pushed to this channel by the main events-gathering routine
+	Notifier chan []byte
+
+	// New client connections
+	newClients chan chan []byte
+
+	// Closed client connections
+	closingClients chan chan []byte
+
+	// Client connections registry
+	clients map[chan []byte]bool
+}
+
+// Listen on different channels and act accordingly
+func (broker *Broker) listen() {
+	for {
+		select {
+		case s := <-broker.newClients:
+			// A new client has connected.
+			// Register their message channel
+			broker.clients[s] = true
+			log.Printf("Client added. %d registered clients", len(broker.clients))
+
+		case s := <-broker.closingClients:
+			// A client has dettached and we want to
+			// stop sending them messages.
+			delete(broker.clients, s)
+			log.Printf("Removed client. %d registered clients", len(broker.clients))
+
+		case event := <-broker.Notifier:
+			// We got a new event from the outside!
+			// Send event to all connected clients
+			for clientMessageChan, _ := range broker.clients {
+				clientMessageChan <- event
+			}
+		}
+	}
+}
+
+// Broker factory
+func NewServer() (broker *Broker) {
+	// Instantiate a broker
+	broker = &Broker{
+		Notifier:       make(chan []byte, 1),
+		newClients:     make(chan chan []byte),
+		closingClients: make(chan chan []byte),
+		clients:        make(map[chan []byte]bool),
+	}
+
+	// Set it running - listening and broadcasting events
+	go broker.listen()
+
+	return
+}
+
+func (b *Broker) ServeHTTP(rw http.ResponseWriter, req  *http.Request) {
+	flusher, ok := rw.(http.Flusher)
+
+	if !ok {
+		http.Error(rw, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+
+	// Set the headers related to event streaming.
+	rw.Header().Set("Content-Type", "text/event-stream")
+	rw.Header().Set("Cache-Control", "no-cache")
+	rw.Header().Set("Connection", "keep-alive")
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// Each connection registers its own message channel with the Broker's connections registry
+	messageChan := make(chan []byte)
+
+	// Signal the broker that we have a new connection
+	b.newClients <- messageChan
+
+	// Listen to connection close and un-register messageChan
+	notify := req.Context().Done()
+
+	go func() {
+		<-notify
+		b.closingClients <- messageChan
+	}()
+
+	// block waiting for messages broadcast on this connection's messageChan
+	for {
+		// Write to the ResponseWriter
+		// Server Sent Events compatible
+		fmt.Fprintf(rw, "data: %s\n\n", <-messageChan)
+
+		// Flush the data immediatly instead of buffering it for later.
+		flusher.Flush()
+	}
+}
+
+func main() {
+
+	broker := NewServer()
+
+	go func() {
+		for {
+			time.Sleep(time.Second * 2)
+			eventString := fmt.Sprintf("the time is %v", time.Now())
+			log.Println("Receiving event")
+			broker.Notifier <- []byte(eventString)
+		}
+	}()
+
+	log.Fatal("HTTP server error: ", http.ListenAndServe("localhost:3000", broker))
+
+}
